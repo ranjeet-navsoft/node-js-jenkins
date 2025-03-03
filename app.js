91862938a239bcc707909269c257679f093a76ab
ranjeet-navsoft <ranjeet.sahoo@navsoft.in>
2025-03-03 17:14:41 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ app.delete("/users/:id", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 module.exports = app;
