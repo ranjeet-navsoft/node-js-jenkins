4e5d5b9c46f3cc5c77bfe0981ff30fb840f63f5a
ranjeet-navsoft <ranjeet.sahoo@navsoft.in>
2025-03-03 16:16:33 +0530

    my first node js app

---
+++ app.js
@@ -0,0 +1,79 @@
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Sample Data
+let users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+];
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the Demo API!");
+});
+
+// Get all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Get user by ID
+app.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  user ? res.json(user) : res.status(404).send("User not found");
+});
+
+// Add a new user
+app.post("/users", (req, res) => {
+  const newUser = { id: users.length + 1, name: req.body.name };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// Update a user
+app.put("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (user) {
+    user.name = req.body.name;
+    res.json(user);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+// Delete a user
+app.delete("/users/:id", (req, res) => {
+  users = users.filter((u) => u.id !== parseInt(req.params.id));
+  res.send("User deleted");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
